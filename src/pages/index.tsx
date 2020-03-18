795f18a2f81feb7ec51b7240a8b741737f571fc9
mottox2 <y.mottox2@gmail.com>
2020-03-18 13:54:28 +0900

    Add meta tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,47 @@
+import Head from "next/head";
+
 import { ViewPager, usePager } from "../components/ViewPager";
 import { Start } from "../patterns/Start";
 import { Physics } from "../patterns/Physics";
+import { SlowAd } from "../patterns/SlowAd";
 
 import styles from "./index.module.css";
 
 const Home = () => {
-  const pagerProps = usePager(5, 0);
+  const pagerProps = usePager(5, 2);
   const { paginate } = pagerProps;
   const toNext = () => paginate(1);
+
+  const title = "Bad UI for developers";
+  const description =
+    "開発者のために良くないUIを用意しました。実際に体験しながら良いUIについて考えてみましょう。";
 
   // pageが変わったら、hashつけるやつ
   return (
     <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} key="description" />
+        <meta name="keywords" content="UI, React" key="keywords" />
+        <meta
+          property="og:description"
+          content={description}
+          key="description"
+        />
+        <meta property="og:type" content="website" key="type" />
+        <meta name="twitter:title" content={title} key="twitter:title" />
+        <meta
+          name="twitter:description"
+          content={description}
+          key="twitter:description"
+        />
+        <meta
+          name="twitter:card"
+          content="summary_large_image"
+          key="twitter:card"
+        />
+        <meta name="twitter:creator" content="@mottox2" key="twitter:creater" />
+      </Head>
       <div className={styles.navigationBar}>
         <button className={styles.toPrev} onClick={() => paginate(-1)}>
           Prev
